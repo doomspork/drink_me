f1b4a7059b356e4ca334b445c51040138077e84c
Sean Callan <seancallan@gmail.com>
2015-05-10 10:23:14 -0700

    Include API key in Account responses

--- test/controllers/account_controller_test.exs
+++ test/controllers/account_controller_test.exs
@@ -2,8 +2,9 @@ defmodule DrinkMe.AccountControllerTest do
   use DrinkMe.ConnCase
 
   alias DrinkMe.Account
-  @valid_params account: %{aws_access_id: "some content", aws_secret_key: "some content", name: "some content"}
+  @valid_params account: %{aws_access_id: "new key", aws_secret_key: "new secret", email: "test@example.com"}
   @invalid_params account: %{}
+  @minimal_account %Account{aws_access_id: "old key", aws_secret_key: "old secret", email: "test@example.com" }
 
   setup do
     conn = conn() |> put_req_header("accept", "application/json")
@@ -16,18 +17,20 @@ defmodule DrinkMe.AccountControllerTest do
   end
 
   test "GET /accounts/:id", %{conn: conn} do
-    account = Repo.insert %Account{}
+    account = Repo.insert @minimal_account
     conn = get conn, account_path(conn, :show, account)
     assert json_response(conn, 200)["account"] == %{
-      "id" => account.id,
-      "name" => account.name,
-      "access_id" => account.aws_access_id
+      "access_id" => account.aws_access_id,
+      "api_key" => account.api_key,
+      "email" => account.email,
+      "id" => account.id
     }
   end
 
   test "POST /accounts with valid data", %{conn: conn} do
     conn = post conn, account_path(conn, :create), @valid_params
     assert json_response(conn, 200)["account"]["id"]
+    assert json_response(conn, 200)["account"]["api_key"]
   end
 
   test "POST /accounts with invalid data", %{conn: conn} do
@@ -36,19 +39,14 @@ defmodule DrinkMe.AccountControllerTest do
   end
 
   test "PUT /accounts/:id with valid data", %{conn: conn} do
-    account = Repo.insert %Account{}
+    account = Repo.insert @minimal_account
     conn = put conn, account_path(conn, :update, account), @valid_params
     assert json_response(conn, 200)["account"]["id"]
-  end
-
-  test "PUT /accounts/:id with invalid data", %{conn: conn} do
-    account = Repo.insert %Account{}
-    conn = put conn, account_path(conn, :update, account), @invalid_params
-    assert json_response(conn, 422)["errors"] != %{}
+    assert json_response(conn, 200)["account"]["access_id"] == "new key"
   end
 
   test "DELETE /accounts/:id", %{conn: conn} do
-    account = Repo.insert %Account{}
+    account = Repo.insert @minimal_account
     conn = delete conn, account_path(conn, :delete, account)
     assert json_response(conn, 200)["account"]["id"]
     refute Repo.get(Account, account.id)
--- web/views/account_view.ex
+++ web/views/account_view.ex
@@ -10,6 +10,6 @@ defmodule DrinkMe.AccountView do
   end
 
   def render("account.json", %{account: account}) do
-    %{id: account.id, name: account.name, access_id: account.aws_access_id}
+    %{id: account.id, email: account.email, access_id: account.aws_access_id, api_key: account.api_key}
   end
 end
